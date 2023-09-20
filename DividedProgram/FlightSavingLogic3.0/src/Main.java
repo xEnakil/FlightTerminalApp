438d6bda9d8987539f7a45cf7cb225e63a70534f
Elmin Mugalov <elmingameboy@Gmail.com>
2023-09-21 01:31:12 +0400

    :D

--- DividedProgram/FlightSavingLogic3.0/src/Main.java
+++ DividedProgram/FlightSavingLogic3.0/src/Main.java
@@ -1,16 +1,17 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
         while (true) {
+
+            //writing code from https://stackoverflow.com/questions/1377279/find-a-line-in-a-file-and-remove-it
+            //adding a method that checks if temp file exist and if exist it will rename it to the older version name;
+
             System.out.println("-------------------------------");
-            System.out.println("1. Add Flights\n2. Show Flights\n3. Exit");
+            System.out.println("1. Add Flights\n2. Show Flights\n3. Delete Flight.\n4. Exit");
             System.out.println("-------------------------------");
             System.out.print("Your choice: ");
             int choice = input.nextInt();
@@ -25,10 +26,12 @@ public class Main {
                     ArrayList<Flight> listFlight = readFlightFromFile();
                     showFlights(listFlight);
                     break;
-                case 3:
+                case 4:
                     System.out.println("Thanks for using our software. Leaving the system....");
                     System.exit(0);
                     break;
+                case 3:
+                    removeFlight(input);
                 default:
                     System.out.println("No such choice");
             }
@@ -37,7 +40,7 @@ public class Main {
 
     public static void addFlightsToFile (Scanner input) {
         ArrayList<String> flightDetails = new ArrayList<>();
-        System.out.print("Enter the id:");
+        System.out.print("Enter the id: ");
         String id = input.next();
         System.out.print("Enter destination from: ");
         String destinationFrom = input.next();
@@ -92,6 +95,46 @@ public class Main {
         for (Flight flight : flightList) {
             System.out.println("Id: " + flight.id + "\nDestination From: " + flight.destinationFrom + "\nDestination To: " + flight.destinationTo + "\nSeat Count: " + flight.seatCount);
             System.out.println("-------------------------------");
+        }
+    }
+
+    public static void removeFlight (Scanner input) throws IOException {
+        String fileName = "flights.txt";
+        String lineToRemove = input.next();
+
+        try {
+            File inputFile = new File(fileName);
+            File tempFile = new File("tempFile.txt");
+
+            BufferedReader br = new BufferedReader(new FileReader(inputFile));
+            BufferedWriter bw = new BufferedWriter(new FileWriter(tempFile));
+
+            String currentLine;
+
+            while ((currentLine = br.readLine()) != null) {
+                if (currentLine.contains(lineToRemove)) {
+                    continue;
+                }
+                bw.write(currentLine);
+                bw.newLine();
+            }
+
+            bw.close();
+            br.close();
+
+            if (inputFile.delete()) {
+                if (tempFile.renameTo(inputFile)) {
+                    System.out.println("Line deleted successfully!");
+                } else {
+                    System.out.println("Error renaming the temporary file.");
+                }
+            } else {
+                System.out.println("Error deleting the file.");
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
